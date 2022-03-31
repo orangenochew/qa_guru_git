4ebeae89ab20cb4da8c0aa1a39be880e4b3bbf1c
Ekaterina Tsyganova <78601051+orangenochew@users.noreply.github.com>
2022-03-31 14:14:58 +0300

    Update Main.java

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -6,4 +6,8 @@ public class Main {
         System.out.println("Hello, World!");
         System.out.println("Make love not war");
     }
+    
+    public static void (){
+        System.out.println("Another method");
+    }
 }
